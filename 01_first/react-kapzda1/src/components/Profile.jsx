967c1afa94429e18d484a41476ab5a5e33a24cd0
Rox <79581077853@mail.ru>
2021-06-12 16:26:18 +0200

    new file

---
+++ 01_first/react-kapzda1/src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React, { Component} from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src=' https://img.freepik.com/free-photo/laptop-and-books-copy-space_23-2148475431.jpg?size=626&ext=jpg'/>
+        </div>
+
+
+        <div>
+            ava + descriptoin
+        </div>
+        <div>
+            My post
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                post2
+            </div>
+        </div>
+
+    </div>
+}
+
+export default Profile;
