f3144ddf5228d93371e871f64bc54913ddb3d66f
Rox <79581077853@mail.ru>
2021-06-17 22:01:17 +0200

    17.06.2021

--- 01_first/react-kapzda1/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component} from 'react';
-
-const Header = () => {
-    return <header className='header'>
-        <img src='https://cdn-images.threadless.com/threadless-media/artist_shops/shops/dfonseca/products/393920/original-1525530512-4d26e14e3f0c8413e9478503ea36b3bc.png?v=3&d=eyJvbmx5X21ldGEiOiBmYWxzZSwgImZvcmNlIjogZmFsc2UsICJvcHMiOiBbWyJ0cmltIiwgW2ZhbHNlLCBmYWxzZV0sIHt9XSwgWyJyZXNpemUiLCBbXSwgeyJ3aWR0aCI6IDk5Ni4wLCAiYWxsb3dfdXAiOiBmYWxzZSwgImhlaWdodCI6IDk5Ni4wfV0sIFsiY2FudmFzX2NlbnRlcmVkIiwgWzEyMDAsIDEyMDBdLCB7ImJhY2tncm91bmQiOiAiMDAwMDAwIn1dXX0='/>
-    </header>
-}
-
-export default Header;
