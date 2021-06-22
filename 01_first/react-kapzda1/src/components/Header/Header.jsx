ef703b417eadc63147047f1610732dfd60ed030f
sheypuk <78950211+sheypuk@users.noreply.github.com>
2021-06-22 16:11:58 +0200

    22.06

--- 01_first/react-kapzda1/src/components/Header/Header.jsx
+++ 01_first/react-kapzda1/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://cdn-images.threadless.com/threadless-media/artist_shops/shops/dfonseca/products/393920/original-1525530512-4d26e14e3f0c8413e9478503ea36b3bc.png?v=3&d=eyJvbmx5X21ldGEiOiBmYWxzZSwgImZvcmNlIjogZmFsc2UsICJvcHMiOiBbWyJ0cmltIiwgW2ZhbHNlLCBmYWxzZV0sIHt9XSwgWyJyZXNpemUiLCBbXSwgeyJ3aWR0aCI6IDk5Ni4wLCAiYWxsb3dfdXAiOiBmYWxzZSwgImhlaWdodCI6IDk5Ni4wfV0sIFsiY2FudmFzX2NlbnRlcmVkIiwgWzEyMDAsIDEyMDBdLCB7ImJhY2tncm91bmQiOiAiMDAwMDAwIn1dXX0='/>
+        <img src='https://mythemestore.com/beehive-preview/wp-content/themes/beehive/assets/images/logo-icon.svg'/>
     </header>
 }
 
