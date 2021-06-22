9ded3307251e41a4a439dda64bcbedbcf6f4d4ed
sheypuk <78950211+sheypuk@users.noreply.github.com>
2021-06-22 15:29:51 +0200

    22.06

--- 01_first/react-kapzda1/src/components/Dialogs/Dialogs.jsx
+++ 01_first/react-kapzda1/src/components/Dialogs/Dialogs.jsx
@@ -4,9 +4,31 @@ import s from './Dialogs.module.css'
 const Dialogs = (props) => {
 
     return (
-    <div>
-        Dialogs
-    </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' +  s.active}>
+                    Roman
+                </div>
+                <div className={s.dialog}>
+                    Alena
+                </div>
+                <div className={s.dialog}>
+                    Oleg
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Alecsandr
+                </div>
+            </div>
+
+        <div className={s.messages}>
+            <div className={s.dialog}>Hi</div>
+            <div className={s.dialog}>How are your doin</div>
+            <div className={s.dialog}>Yo</div>
+        </div>
+        </div>
     )
 }
 
