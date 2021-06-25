cb6b69f3664ebe61f755344d60e206173880f8cf
sheypuk <78950211+sheypuk@users.noreply.github.com>
2021-06-25 13:37:31 +0200

    25.06.lesson 25_1

--- 01_first/react-kapzda1/src/components/Dialogs/Dialogs.jsx
+++ 01_first/react-kapzda1/src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import s from './Dialogs.module.css'
+import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
+
 
 const DialogItem = (props) => {
 
     let path = "/dialogs/" + props.id;
 
-    return <div className={s.dialog + ' ' + s.active}>
-        <NavLink to={path}>{props.name}</NavLink>
+    return <div className={s.dialog}>
+        <NavLink to={path} className={s.dialog} activeClassName={s.active}>{props.name}</NavLink>
     </div>
 
 }
 
 const Message = (props) => {
-    return <div className={s.dialog}>{props.message}</div>
+    return <div className={s.message}>{props.message}</div>
 }
 
 const Dialogs = (props) => {
