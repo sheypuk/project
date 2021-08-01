427330ed5d6caa9cb5a4918c4f77a87382044b68
Rox <79581077853@mail.ru>
2021-08-01 13:24:34 +0200

    01.08

--- 01_first/react-kapzda1/src/redux/dialogs-reducer.js
+++ 01_first/react-kapzda1/src/redux/dialogs-reducer.js
@@ -2,7 +2,7 @@ const ADD_MESSAGE = 'ADD_MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE_NEW_MESSAGE_TEXT';
 
 
-let initialState =  {
+let initialState = {
     dialogs: [
         {id: 1, name: 'Dimych'},
         {id: 2, name: 'Alena'},
@@ -21,23 +21,26 @@ let initialState =  {
     newMessageText: ''
 };
 
-const dialogsReducer = (state = initialState , action) => {
+const dialogsReducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case UPDATE_NEW_MESSAGE_TEXT:
-            state.newMessageText = action.newMessage;
+        case UPDATE_NEW_MESSAGE_TEXT: {
+            let stateCopy = {...state}
+            stateCopy.newMessageText = action.newMessage;
             return state;
-
-        case ADD_MESSAGE:
+        }
+        case ADD_MESSAGE: {
             let newMessage = {
                 id: 6,
                 message: state.newMessageText,
 
             };
-            state.messages.push(newMessage);
-            state.newMessageText = '';
-            return state;
-
+            let stateCopy = {...state}
+            stateCopy.messages = [...state.messages]
+            stateCopy.messages.push(newMessage);
+            stateCopy.newMessageText = '';
+            return stateCopy;
+        }
         default:
             return state;
     }
--- 01_first/react-kapzda1/src/redux/profile-reducer.js
+++ 01_first/react-kapzda1/src/redux/profile-reducer.js
@@ -12,19 +12,23 @@ let initialstate = {
 
 const profileReducer = (state= initialstate , action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likeCount: 0
             };
-
-            state.posts.push(newPost);
-            state.newPostText = '';
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText;
-            return state;
+            let stateCopy = {...state}
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost)
+            stateCopy.newPostText = '';
+            return stateCopy;
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy = {...state}
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
     }
