053ee6f4097e0b46260ee276698d490900df16b2
Rox <sheypuk@yandex.ru>
2021-09-13 19:18:51 +0200

    13.09

--- 01_first/react-kapzda1/src/components/Users/Users.jsx
+++ 01_first/react-kapzda1/src/components/Users/Users.jsx
@@ -5,12 +5,12 @@ import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component{
 
-    constructor(props) {
-        super(props);
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
 
-            })
+
+    componentDidMount() {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        this.props.setUsers(response.data.items)
+    });
 
     }
 
