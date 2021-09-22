c39c8ca54d0a079dfa9e5967f52e2c7ecf2af93b
Rox <sheypuk@yandex.ru>
2021-09-22 20:04:43 +0200

    22.09

---
+++ 01_first/react-kapzda1/src/components/Common/Preloader/Preloader.js
@@ -0,0 +1,10 @@
+import preloader from "../../../assets/images/loader.gif";
+import React from "react";
+
+let Preloader = (props) => {
+    return <div>
+        <img src={preloader} />
+    </div>
+}
+
+export default Preloader;
